1299890789730495a9810e858a502684501831fe
Inove Ws - Paulo Ricardo <inovewebsites.com@gmail.com>
2017-06-23 13:33:57 -0400

    blablabla

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,10 +13,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+        if(env('APP_ENV') !== 'local')
+        {
+            $url->forceSchema('https');
+        }
     }
 
     /**
