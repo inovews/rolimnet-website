ec53561a6751adf9b4218dd2d488fcca4f4760c8
Inove Ws - Paulo Ricardo <godoi.pauloricardo@gmail.com>
2019-02-01 09:46:42 -0400

    010220190946

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -172,7 +172,7 @@
                                             <div class="pricing-list-v4-content">
                                                 <div class="margin-b-40">
                                                     <span class="pricing-list-v4-price-sign">R$</span>
-                                                    <span class="pricing-list-v4-price">{{ $plan->price }},</span>
+                                                    <span class="pricing-list-v4-price">{{ $plan->price }}</span>
                                                     <span class="pricing-list-v4-price-info">mês</span>
                                                 </div>
                                                 <div class="center-block">
